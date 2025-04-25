f2bb990fc97d522c72fad23808d099195c33d103
VaiseIamHere <singhvaibhav12062005@gmail.com>
2025-04-26 02:42:21 +0530

    .

--- test1.py
+++ test1.py
@@ -17,7 +17,7 @@ num_classes = 5
 # print(checkpoints)
 
 data_transform = transforms.Compose([
-    # transforms.Resize((224, 224)),
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
