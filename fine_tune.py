93831ab40e2d4c4b090135d7cf840a53e5ef2ed0
VaiseIamHere <singhvaibhav12062005@gmail.com>
2025-04-26 13:11:54 +0530

    Fine tuning on Retina Mnist

---
+++ fine_tune.py
@@ -0,0 +1,167 @@
+import os
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+
+import sys
+import json
+import torch
+import torch.nn as nn
+from torchvision import transforms, datasets
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+from MedMamba import VSSM as medmamba
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using {device} device.")
+    os.makedirs("/kaggle/working/checkpoints", exist_ok=True)
+
+    # Transforms
+    data_transform = {
+        "train": transforms.Compose([
+            # transforms.Resize((224, 224)),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(10),
+            # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]),
+        "val": transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    }
+
+    # Directories
+    train_dir = "/kaggle/input/mnistdata/extracted_images/train"
+    val_dir = "/kaggle/input/drdataset/dataset/Testing"  # <<< Add your validation folder here
+
+    # Datasets
+    train_dataset = datasets.ImageFolder(root=train_dir, transform=data_transform["train"])
+    val_dataset = datasets.ImageFolder(root=val_dir, transform=data_transform["val"])
+
+    train_num = len(train_dataset)
+    val_num = len(val_dataset)
+
+    # Save class index mapping
+    class_to_idx = train_dataset.class_to_idx
+    idx_to_class = {v: k for k, v in class_to_idx.items()}
+
+    batch_size = int(sys.argv[2])
+    num_workers = int(sys.argv[3])
+    print(f"Batch Size: {batch_size}, Dataloader Workers: {num_workers}")
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
+
+    # Model, loss, optimizer, scheduler
+    num_classes = len(class_to_idx)
+    net = medmamba(num_classes=num_classes, activationOption=sys.argv[1]).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=1e-4)
+    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3, verbose=True)
+    model_path = '/kaggle/input/medmambaimp/pytorch/default/1/mamba_inputscaledgelu_checkpoint89.pth'
+    f = torch.load(model_path, weights_only=True)
+    net.load_state_dict(f["model_state_dict"], strict=True)
+
+    # Save model details
+    model_name = "mamba_tuned"
+    file_path = f"/kaggle/working/{model_name}_details.json"
+
+    if os.path.exists(file_path):
+        with open(file_path, "r") as f:
+            model_details = json.load(f)
+    else:
+        model_details = {
+            "model_name": model_name,
+            "training_params": {
+                "loss": "CrossEntropy",
+                "optimizer": "Adam",
+                "batch_size": batch_size,
+                "activationOption": sys.argv[1],
+                "num_workers": num_workers
+            },
+            "metrics": []
+        }
+
+    epochs = 100
+    train_steps = len(train_loader)
+    val_steps = len(val_loader)
+
+    for epoch in range(epochs):
+        print(f"\nEpoch {epoch+1}/{epochs} started", flush=True)
+        
+        # Training
+        net.train()
+        running_loss = 0.0
+        correct_train = 0
+        total_train = 0
+
+        for images, labels in tqdm(train_loader, desc="Training"):
+            optimizer.zero_grad()
+            images, labels = images.to(device), labels.to(device)
+            outputs = net(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            preds = outputs.argmax(dim=1)
+            correct_train += (preds == labels).sum().item()
+            total_train += labels.size(0)
+
+        avg_train_loss = running_loss / train_steps
+        train_acc = correct_train / total_train
+
+        # Validation
+        net.eval()
+        val_loss = 0.0
+        correct_val = 0
+        total_val = 0
+        with torch.no_grad():
+            for images, labels in tqdm(val_loader, desc="Validation"):
+                images, labels = images.to(device), labels.to(device)
+                outputs = net(images)
+                loss = criterion(outputs, labels)
+
+                val_loss += loss.item()
+                preds = outputs.argmax(dim=1)
+                correct_val += (preds == labels).sum().item()
+                total_val += labels.size(0)
+
+        avg_val_loss = val_loss / val_steps
+        val_acc = correct_val / total_val
+
+        # Step scheduler
+        scheduler.step(avg_val_loss)
+
+        # Save metrics
+        model_details["metrics"].append({
+            "epoch": epoch + 1,
+            "train_acc": train_acc,
+            "train_loss": avg_train_loss,
+            "val_acc": val_acc,
+            "val_loss": avg_val_loss
+        })
+
+        # Save checkpoint
+        if (epoch + 1) % 10 == 0:
+            checkpoint_path = f"/kaggle/working/checkpoints/{model_name}_checkpoint{epoch+1}.pth"
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': net.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'train_loss': avg_train_loss,
+                'val_loss': avg_val_loss
+            }, checkpoint_path)
+
+            with open(file_path, "w") as f:
+                json.dump(model_details, f, indent=4)
+
+        print(f"[Epoch {epoch+1}] Train Loss: {avg_train_loss:.4f}, Train Acc: {train_acc:.4f}, Val Loss: {avg_val_loss:.4f}, Val Acc: {val_acc:.4f}")
+
+if __name__ == '__main__':
+    main()
---
+++ testtune.py
@@ -0,0 +1,92 @@
+import torch
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+import sys
+from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix
+import numpy as np
+import json
+import gc
+
+from MedMamba import VSSM as medmamba
+
+num_classes = 5
+checkpoints = [10*i for i in range(1, 11)]
+
+print("CheckPoints: ", checkpoints)
+
+data_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+test_dir = "/kaggle/input/mnistdata/extracted_images/test"
+test_dataset = datasets.ImageFolder(root=test_dir, transform=data_transform)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=0, pin_memory=True)
+
+test_num = len(test_dataset)
+print(f"Loaded {test_num} test images.")
+metrics = []
+
+activationOption = sys.argv[1]
+
+for checkpoint in checkpoints:
+    print(f"Checkpoint: {checkpoint}, ", end=" ", flush=True)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    net = medmamba(num_classes=num_classes, activationOption=activationOption)
+    net = net.to(device)
+
+    # load_path = f"/kaggle/input/{notebook_name}/checkpoints/mamba_{activationOption}_checkpoint{checkpoint}.pth"
+    model_path = f"/kaggle/working/checkpoints/mamba_tuned_checkpoint{checkpoint}.pth"
+    f = torch.load(model_path, weights_only=True)
+    net.load_state_dict(f["model_state_dict"], strict=True)
+    net.eval()
+
+    all_labels = []
+    all_predictions = []
+
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+
+            all_labels.extend(labels.cpu().numpy())
+            all_predictions.extend(predicted.cpu().numpy())
+
+    accuracy = 100 * np.sum(np.array(all_predictions) == np.array(all_labels)) / test_num
+
+    precision = precision_score(all_labels, all_predictions, average='weighted', zero_division=0)
+    recall = recall_score(all_labels, all_predictions, average='weighted', zero_division=0)
+    f1 = f1_score(all_labels, all_predictions, average='weighted', zero_division=0)
+
+    conf_matrix = confusion_matrix(all_labels, all_predictions)
+
+    print("All Predictions: ")
+    print(all_predictions)
+
+    print("All Labels: ")
+    print(all_labels)
+
+    print(f"Test Accuracy: {accuracy:.2f}%", flush=True)
+    print("Precison: ", precision)
+    print("Recall: ", recall)
+    print("F1: ", f1)
+    print("Conf Matrix ->")
+    print(conf_matrix)
+
+    metrics.append({
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1_score": f1,
+        "conf_matrix": conf_matrix.tolist(),
+    })
+
+    del net
+    torch.cuda.empty_cache()
+    gc.collect()
+
+json_path = f"/kaggle/working/mnist_tuned_metrices.json"
+with open(json_path, 'w') as f:
+    json.dump(metrics, f, indent=4)
